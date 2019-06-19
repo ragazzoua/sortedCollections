97bd88c2ca84cfed07135a76dfa1e506a9804e78
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-06-19 15:16:28 +0300

    Initial commit

---
+++ src/com/company/StockItem.java
@@ -0,0 +1,35 @@
+package com.company;
+
+public class StockItem implements Comparable<StockItem> {
+    private final String name;
+    private double price;
+    private int quantityStock = 0;
+
+    public StockItem(String name, double price) {
+        this.name = name;
+        this.price = price;
+        this.quantityStock = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantityStock() {
+        return quantityStock;
+    }
+
+    public void setPrice(double price) {
+        if (price > 0) {
+            this.price = price;
+        }
+    }
+
+    public void adjustStock(int quantity){
+
+    }
+}
