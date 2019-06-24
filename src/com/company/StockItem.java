52c1ca489b9df100a22437022ce228ddc7a23ab4
Serhii Miroshnichenko <ragazzo@ukr.net>
2019-06-24 10:45:50 +0300

    compare to, hashcode, toString implemented

--- src/com/company/StockItem.java
+++ src/com/company/StockItem.java
@@ -29,7 +29,45 @@ public class StockItem implements Comparable<StockItem> {
         }
     }
 
-    public void adjustStock(int quantity){
+    public void adjustStock(int quantity) {
+        int newQuantity = this.quantityStock + quantity;
+        if (newQuantity >= 0) {
+            this.quantityStock = newQuantity;
+        }
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        System.out.println("Entering StockItem.equals");
+        if (obj == this) {
+            return true;
+        }
+        if ((obj == null) || (obj.getClass() != this.getClass())) {
+            return false;
+        }
+        String objName = ((StockItem) obj).getName();
+        return this.name.equals(objName);
+    }
+
+    @Override
+    public int hashCode() {
+        return this.name.hashCode() + 31;
+    }
+
+    @Override
+    public int compareTo(StockItem o) {
+        System.out.println("Entering StockItem.compareTo");
+        if (this == o) {
+            return 0;
+        }
+        if (o != null) {
+            return this.name.compareTo(o.getName());
+        }
+        throw new NullPointerException();
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " : price " + this.price;
     }
 }
